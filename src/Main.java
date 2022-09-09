34f84fa1c753305533c9560ae3a42c89b3832485
Luis Felipe <luisfelsie28@gmail.com>
2022-09-08 20:05:19 -0500

    nuevo reto completado

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        MultiplesOf3Or5 reto = new MultiplesOf3Or5();
-        System.out.println( reto.exec(10));
+        PlayingWithDigits reto = new PlayingWithDigits();
+        System.out.println( reto.exec(462888788 , 3));
 
     }
 }
---
+++ src/PlayingWithDigits.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class PlayingWithDigits {
+
+    public int exec(Integer number, Integer base){
+            int totalPow = powRun(number.toString().split(""), base);
+            boolean funnyPropertie = true;
+            for (int exponent = 1; funnyPropertie; exponent++){
+                long valFunnyNum =  number*exponent;
+                if (totalPow == valFunnyNum){
+                    return exponent;
+                }
+                else if (valFunnyNum>totalPow){
+                    return -1;
+                }
+            }
+            return -1;
+        }
+        /**
+         *Method used to return the pow of every digit
+         * Example
+         * numberSplitted = [1,2,3,4,5,6]
+         * base = 3
+         *
+         * @operation =  (1**2) + (2**3) + (3**4) + (4**5) + (5**6);
+         *
+         * @return int
+         * @params numberSplitted array from the digits of the numbers
+         * @params base, base of the pow where will begin
+         */
+        private int powRun (String[] numberSplited, Integer base){
+
+            int tmp = 0;
+            int numberIndex = 0;
+            for (Integer i = base; i<numberSplited.length+base; i++){
+                Double digit = Double.parseDouble(numberSplited[numberIndex]);
+                tmp += Math.pow(digit,i);
+                numberIndex++;
+            }
+            return tmp;
+        }
+}
